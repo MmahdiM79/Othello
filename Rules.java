74f18a57f9e16df7c62c7c44f04af16801e0e830
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-03-31 16:36:41 +0430

    bugs fixed

--- Rules.java
+++ Rules.java
@@ -13,18 +13,19 @@ public class Rules
 
     
     private static HashMap<Integer, ArrayList<String>> playersSelectableBlocks = new HashMap<>();
-    private static HashMap<Integer, ArrayList<String>> playersBlocks = new HashMap<>();
 
 
     public static void initialization()
     {
-        playersBlocks.put(1, new ArrayList<String>());
-        playersBlocks.put(-1, new ArrayList<String>());
-
         playersSelectableBlocks.put(1, new ArrayList<String>());
         playersSelectableBlocks.put(-1, new ArrayList<String>());
     }
     
+    public static void reset(int player)
+    {
+        playersSelectableBlocks.remove(player);
+        playersSelectableBlocks.put(player, new ArrayList<String>());
+    }
 
     public static void findSelectableBlocks(Board gameBoard, int player)
     {
@@ -34,8 +35,11 @@ public class Rules
                 if (gameBoard.getMainBoard()[j][i] == (-player) && isEmptyAround(gameBoard, j, i))
                 {
                     for (int deltaY = -1; deltaY < 2; deltaY++)
-                        for (int deltaX = -1; deltaX < 2; deltaX ++)
+                        for (int deltaX = -1; deltaX < 2; deltaX++)
                         {
+                            if (!isInRange(j+deltaY, i+deltaX))
+                                continue;
+                                
                             if (gameBoard.getMainBoard()[j+deltaY][i+deltaX] == 0)
                                 selectBlock(gameBoard, j+deltaY, i+deltaX, player);
 
@@ -50,158 +54,47 @@ public class Rules
     {
         for (int j = -1; j < 2; j++)
             for (int i = -1; i < 2; i++)
+            {
+                if (!isInRange(y+j, x+i))
+                    continue;
+
                 if (gameBoard.getMainBoard()[y+j][x+i] == 0)
                     return true;
+            }
 
         return false;
     }
 
 
+
     private static void selectBlock(Board gameBoard, int y, int x, int player)
     {
-        for (int j = -1, len = 0; 0 <= y+j; j--)
-        {
-            if (gameBoard.getMainBoard()[y+j][x] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x] == player)
+        int increasement_j, increasement_i;
+        
+        for (int J = -1; J < 2; J++)
+            for (int I = -1; I < 2; I++)
             {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
+                increasement_j = (int)Math.signum(J);
+                increasement_i = (int)Math.signum(I);
+
+                for (int j = J, i = I, len = 0; isInRange(y+j, x+i); j+=increasement_j, i+=increasement_i)
+                {
+                    if (gameBoard.getMainBoard()[y+j][x+i] == 0 || gameBoard.getMainBoard()[y+j][x+i] == 2)
+                        break;
+
+                    else if (gameBoard.getMainBoard()[y+j][x+i] == player)
+                    {
+                        if (len == 0)
+                            break;
+                        
+                        playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
+                        gameBoard.changeBoard(y, x, 2);
+                        return;
+                    }
+
+                len++;
+                }
             }
-
-            len++;
-        }
-
-        for (int j = 1, len = 0; y+j < 8; j++)
-        {
-            if (gameBoard.getMainBoard()[y+j][x] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int i = -1, len = 0; 0 <= x+i; i--)
-        {
-            if (gameBoard.getMainBoard()[y][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int i = 1, len = 0; x+i < 8; i++)
-        {
-            if (gameBoard.getMainBoard()[y][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int j = 1, i = 1, len = 0; (y+j < 8)&&(x+i < 8); j++, i++)
-        {
-            if (gameBoard.getMainBoard()[y+j][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int j = -1, i = -1, len = 0; (0 <= y+j)&&(0 <= x+i); j--, i--)
-        {
-            if (gameBoard.getMainBoard()[y+j][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int j = -1, i = 1, len = 0; (0 <= y+j)&&(x+i < 8); j--, i++)
-        {
-            if (gameBoard.getMainBoard()[y+j][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-            len++;
-        }
-
-        for (int j = 1, i = -1, len = 0; (y+j < 8)&&(0 <= x+i); j++, i--)
-        {
-            if (gameBoard.getMainBoard()[y+j][x+i] == 0)
-                break;
-
-            else if (gameBoard.getMainBoard()[y+j][x+i] == player)
-            {
-                if (len == 0)
-                    break;
-                
-                playersSelectableBlocks.get(player).add("" + (y+1) + ((char) (x+65)));
-                gameBoard.changeBoard(y, x, 2);
-                return;
-            }
-
-           len++;
-        }
     }
 
 
@@ -249,84 +142,45 @@ public class Rules
         return -1;
     }
 
-    public static void applyChoose(Board gameBoard, int player, String choosenBlock)
+    private static boolean isInRange(int y, int x)
+    {
+        return ((y < 8 && x < 8) && (0 <= y && 0 <= x));
+    }
+
+    public static int applyChoose(Board gameBoard, int player, String choosenBlock)
     {  
+        int gainedScore = 0;
+
         int y = deCode(choosenBlock, 'Y');
         int x = deCode(choosenBlock, 'X');
 
         System.out.println(y + " " + x);
 
         gameBoard.changeBoard(y, x, player);
+        gainedScore++;
 
 
-        int j = 0, i = 0;
+        int j, increasement_j, i, increasement_i;
 
-        j = -1;
-        i = 0;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j--;
-        }
+        for (int J = -1; J < 2; J++)
+            for (int I = -1; I < 2; I++)
+            {
+                increasement_j = (int)Math.signum(J);
+                increasement_i = (int)Math.signum(I);
 
-        j = 1;
-        i = 0;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j++;
-        }
+                j = J; 
+                i = I;
+                while (isInRange(y+j, x+i) && gameBoard.getMainBoard()[y+j][x+i] == (-player))
+                {
+                    gameBoard.changeBoard(y+j, x+i, player);
+                    gainedScore++;
 
-        j = 0;
-        i = -1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            i--;
-        }
+                    j += increasement_j;
+                    i += increasement_i;
+                }
+            }
 
-        j = 0;
-        i = 1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            i++;
-        }
-
-        j = -1;
-        i = -1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j--;
-            i--;
-        }
-
-        j = 1;
-        i = 1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j++;
-            i++;
-        }
-
-        j = -1;
-        i = 1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j--;
-            i++;
-        }
-
-        j = 1;
-        i = -1;
-        while (gameBoard.getMainBoard()[y+j][x+i] == (-player))
-        {
-            gameBoard.changeBoard(y+j, x+i, player);
-            j++;
-            i--;
-        }
+        
+        return gainedScore;
      }
 }
