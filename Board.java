a88f475f32d1353ed7ded4cd823e9df74bb6d26c
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-03-31 08:29:04 +0430

    Board class finished. (version 0.1.0)

--- Board.java
+++ Board.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
-
-interface BoardActs
-{
-
-}
 
 /**
+ * This class repersent a simple Othello board.
+ * It holds to board: 
+ *        1.mainBoard: a simple 8x8 int array that shown the blocks status.
+ *        2.visualBoard a 33x57 char array which is synchronized with the board. this board will be showen to the client
+ * 
+ * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.0.2
+ * @version 0.1.0
  */
-public class Board implements BoardActs
+public class Board
 {
 
             /* Feilds */
@@ -25,11 +25,12 @@ public class Board implements BoardActs
 
 
     // this array will be showen in standard output (terminal).
-    // X: player 1,  O: player 2
+    // X: player 1 -> red color,  O: player 2 -> cyan color
     private char[][] visualBoard; 
 
     // this array is for perform calculations and decisions
-    private int[][] board;
+    // 1 refers to player1 (X: Red),  -1 refers to player2 (O: Cyan),  0 refers to a empty block
+    private int[][] mainBoard;
 
 
 
@@ -37,8 +38,18 @@ public class Board implements BoardActs
 
           /* Constructor */
 
+    /**
+     * Creat a new Board
+     */
     public Board()
     {
+        // fill the main board
+        mainBoard = new int[8][8];
+        for (int j = 0; j < 8; j++)
+            for (int i = 0; i < 8; i++)
+                mainBoard[j][i] = 0;
+
+
         visualBoard = new char[VISUAL_BOARD_Y][VISUAL_BOARD_X];
         makeVisualBoard();
 
@@ -47,29 +58,41 @@ public class Board implements BoardActs
 
         applyToVisualBoard(3, 4, 'O');
         applyToVisualBoard(4, 3, 'O');
-
     }
+
+
+
 
 
 
             /* Methods */
 
+
+    // * getter methods *
+
     /**
-     * @return the visualBoard
+     * @return the board 
+     */
+    public int[][] getMainBoard() 
+    {
+        return mainBoard;
+    }
+    /**
+     * @return the visual board
      */
     public char[][] getVisualBoard() 
     {
         return visualBoard;
     }
     /**
-     * @return the vISUAL_BOARD_X
+     * @return the visual board's x
      */
     public int getVisualX() 
     {
         return VISUAL_BOARD_X;
     }
     /**
-     * @return the vISUAL_BOARD_Y
+     * @return the visual board's y
      */
     public int getVisualY() 
     {
@@ -77,6 +100,40 @@ public class Board implements BoardActs
     }
 
 
+
+
+    /**
+     * This method change the board status
+     * 
+     * @param y : the y of the block that you want to change it
+     * @param x : the x ot the block that you want to change it
+     * @param blockKind : the new status of the block. 1 refers to player1 (X: Red),  -1 refers to player2 (O: Cyan),  0 refers to a empty block 
+     */
+    public void changeBoard(int y, int x, int blockKind)
+    {
+        mainBoard[y][x] = blockKind;
+
+
+        char player;
+        switch (blockKind)
+        {
+            case 1:
+                player = 'X';
+            break;
+
+            case -1:
+                player = 'O';
+            break;
+
+            default:
+                player = ' '
+        }
+        applyToVisualBoard(y, x, player);
+    }
+
+
+    // this method make changes in visual board
+    // this method used in changeBoard
     private void applyToVisualBoard(int y, int x, char player)
     {
         y = (4 * y) + 1;
@@ -90,6 +147,7 @@ public class Board implements BoardActs
     }
 
 
+    // this method fill the visual board for the first time
     private void makeVisualBoard()
     {
         for (int j = 0; j < VISUAL_BOARD_Y; j++)
