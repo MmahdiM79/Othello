e808c8002202d323fd3493fa7c202788eeb737f4
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-02 00:20:29 +0430

    Run(Main) class finished. (version 0.2.0)

--- Run.java
+++ Run.java
@@ -3,39 +3,47 @@ import java.util.Scanner;
 
 /**
  * @author Mohammad Mahdi Malmasi
- * @version 0.1.0
+ * @version 0.2.0
  */
 public class Run
 {
+    // for get the players inputs
+    private static Scanner inputs = new Scanner(System.in);
+
+
+
+    // this method check the player input
     private static boolean isValid(String choosenBlock)
     {
         return ((int)choosenBlock.charAt(0) < 58 && 48 < (int)choosenBlock.charAt(0))
-                &&
+                && 
                 ((int)choosenBlock.charAt(1) < 73 && 64 < (int)choosenBlock.charAt(1));
     }
 
 
-    private static Scanner inputs = new Scanner(System.in);
     public static void main(String[] args) 
     {
+        // initialization classes and needed variables
         Rules.initialization();
         Board gameBoard = new Board();
         Player player1, player2;
+        Bot bot;
+        int operand, scoreDelta = 0;
+        String firstName, choosenBlock = "";
 
-        int operand;
-        String firstName, choosenBlock;
-
-
+        
         while(true)
         {
             while (true)
             {
+                // show the game menu
                 Printer.printMenu();
 
                 operand = inputs.nextInt();
                 inputs.nextLine();
 
-                if (operand < 6 && 0 < operand)
+                // check the player input
+                if (operand < 4 && 0 < operand)
                     break;
             }
 
@@ -43,25 +51,41 @@ public class Run
             {
                 case 1: // new Two Player game
 
+                    // creat a new player for player 1
                     Printer.getPlayerName(1);
                     firstName = inputs.nextLine();
                     player1 = new Player(firstName, "NEVERMIND", 0, 1, 'X', "NEVERMIND");
+                    player1.setScore(2);
                     
+                    // creat a new player for player 2
                     Printer.getPlayerName(2);
                     firstName = inputs.nextLine();
                     player2 = new Player(firstName, "NEVERMIND", 0, -1, 'O', "NEVERMIND");
+                    player2.setScore(2);
 
 
                     while (!Rules.isGameEnded(gameBoard))
                     {
+                        // player 1 turn
                         while (true)
                         {
+                            // show the board to player 1  
                             Rules.findSelectableBlocks(gameBoard, player1.getPlayerID());
                             Printer.printVisualBoard(gameBoard.getVisualBoard(), gameBoard.getVisualY(), gameBoard.getVisualX());
-                            Printer.printTurn(player1);
+                            Printer.printPlayersScores(player1, player2);
 
+                            // check that the player can choose any block or not
+                            if (Rules.isPassed(player1))
+                            {
+                                Printer.passedPlayer(inputs);
+                                break;
+                            }
+
+                            // get player choose
+                            Printer.printTurn(player1);
                             choosenBlock = inputs.nextLine();
 
+                            // check the player input
                             if (choosenBlock.length() > 2 || choosenBlock.length() < 2 || !isValid(choosenBlock))
                             {
                                 Printer.inValidInputError(inputs);
@@ -72,6 +96,7 @@ public class Run
                                 continue;
                             }
 
+                            // check the player choosen move
                             if (!Rules.isPossible(player1.getPlayerID(), choosenBlock))
                             {
                                 Printer.wrongChooseError(inputs);
@@ -83,20 +108,40 @@ public class Run
                             }
 
 
-                            Rules.applyChoose(gameBoard, player1.getPlayerID(), choosenBlock);
-                            Rules.reset(player1.getPlayerID());
-                            gameBoard.reset();
                             break;
                         }
+                        // apply the player choosen move and calculate the player gained score
+                        scoreDelta = Rules.applyChoose(gameBoard, player1.getPlayerID(), choosenBlock);
 
+                        // set the players new scores
+                        player1.setScore(scoreDelta + player1.getScore());
+                        player2.setScore(player2.getScore() - scoreDelta + 1);
+
+                        // reset the map
+                        Rules.reset(player1.getPlayerID());
+                        gameBoard.reset();
+
+
+                        // player 2 turn
                         while (true)
                         {
+                            // show the board to player 2
                             Rules.findSelectableBlocks(gameBoard, player2.getPlayerID());
                             Printer.printVisualBoard(gameBoard.getVisualBoard(), gameBoard.getVisualY(), gameBoard.getVisualX());
-                            Printer.printTurn(player2);
+                            Printer.printPlayersScores(player1, player2);
 
+                            // check that the player can choose any block or not
+                            if (Rules.isPassed(player2))
+                            {
+                                Printer.passedPlayer(inputs);
+                                break;
+                            }
+                            
+                            // get player choose
+                            Printer.printTurn(player2);
                             choosenBlock = inputs.nextLine();
 
+                            // check the player input
                             if (choosenBlock.length() > 3 || choosenBlock.length() == 0 || !isValid(choosenBlock))
                             {
                                 Printer.inValidInputError(inputs);
@@ -107,6 +152,7 @@ public class Run
                                 continue;
                             }
 
+                            // check the player choosen move
                             if (!Rules.isPossible(player2.getPlayerID(), choosenBlock))
                             {
                                 Printer.wrongChooseError(inputs);
@@ -118,26 +164,106 @@ public class Run
                             }
 
 
-                            Rules.applyChoose(gameBoard, player2.getPlayerID(), choosenBlock);
-                            Rules.reset(player2.getPlayerID());
-                            gameBoard.reset();
                             break;
                         }
+                        // apply the player choosen move and calculate the player gained score
+                        scoreDelta = Rules.applyChoose(gameBoard, player2.getPlayerID(), choosenBlock);
+
+                        // set the players new scores
+                        player2.setScore(scoreDelta + player2.getScore());
+                        player1.setScore(player1.getScore() - scoreDelta + 1);
+
+                        /// reset the map
+                        Rules.reset(player2.getPlayerID());
+                        gameBoard.reset();
                     }
+
+                    // show the winner
+                    Rules.winner(player1, player2, inputs);
 
                 break;
                 
-                case 2:
+                case 2: // new Single player game
+
+                    // creat a new player as player ond
+                    Printer.getPlayerName(1);
+                    firstName = inputs.nextLine();
+                    player1 = new Player(firstName, "NEVERMIND", 0, 1, 'X', "NEVERMIND");
+                    player1.setScore(2);
+
+                    // creat a bot 
+                    player2 = new Bot();
+                    player2.setScore(2);
+                    bot = (Bot)player2;
+
+                    while (!Rules.isGameEnded(gameBoard))
+                    {
+                        // player turn
+                        while (true)
+                        {
+                            // show the board to player 
+                            Rules.findSelectableBlocks(gameBoard, player1.getPlayerID());
+                            Printer.printVisualBoard(gameBoard.getVisualBoard(), gameBoard.getVisualY(), gameBoard.getVisualX());
+                            Printer.printPlayersScores(player1, player2);
+
+                            // check that the player can choose any block or not
+                            if (Rules.isPassed(player1))
+                            {
+                                Printer.passedPlayer(inputs);
+                                break;
+                            }
+
+                            // get player choose
+                            Printer.printTurn(player1);
+                            choosenBlock = inputs.nextLine();
+
+                            // check the player input
+                            if (choosenBlock.length() > 2 || choosenBlock.length() < 2 || !isValid(choosenBlock))
+                            {
+                                Printer.inValidInputError(inputs);
+
+                                Rules.reset(player1.getPlayerID());
+                                gameBoard.reset();
+
+                                continue;
+                            }
+
+                            // check the player choosen move
+                            if (!Rules.isPossible(player1.getPlayerID(), choosenBlock))
+                            {
+                                Printer.wrongChooseError(inputs);
+
+                                Rules.reset(player1.getPlayerID());
+                                gameBoard.reset();
+
+                                continue;
+                            }
+
+
+                            break;
+                        }
+                        // apply the player choosen move and calculate the player gained score
+                        scoreDelta = Rules.applyChoose(gameBoard, player1.getPlayerID(), choosenBlock);
+
+                        // set the player and bot new scores
+                        player1.setScore(scoreDelta + player1.getScore());
+                        player2.setScore(player2.getScore() - scoreDelta + 1);
+
+                        // reset the map
+                        Rules.reset(player1.getPlayerID());
+                        gameBoard.reset();
+
+                        // bot move
+                        scoreDelta = bot.botChoose(gameBoard);
+
+                        // set the player and bot new scores
+                        player2.setScore(scoreDelta + player2.getScore());
+                        player1.setScore(player1.getScore() - scoreDelta + 1);
+                    }
                 break;
 
-                case 3:
-                break;
-
-                case 4:
-                break;
-
-                case 5:
-                break;
+                case 3: // exit
+                    return;
             }
         }
     }
